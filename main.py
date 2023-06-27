c7cc1ec98701fa837bad3e2b8c7f49d8843c62fc
isabell austin <iaustin@butler.edu>
2023-06-27 15:00:00 -0400

    6/27

--- main.py
+++ main.py
@@ -10,12 +10,25 @@ def main(blogs) -> None:
     user_blogs = {}
     blogs.get_user_blogs(user_blogs, cnx)
     sites = user_blogs.values()
+    site_paths = list(user_blogs.values())
+    site_ids = list(user_blogs.keys())
+    
     logger.setLevel(logging.INFO)
 
-    for site in sites:
-        plugin_status = blogs.activate_plugin("classic-editor", site)
-        logger.info(f"{site}: {plugin_status}")
- 
+    # plugin = ""
+    # for site in sites:
+        # index = site_paths.index(f"{site}")
+        # site_id = site_ids[index]
+
+        # # for slug in plugin_slug:
+
+        # plugin_status = blogs.activate_plugin(plugin, site, site_id,cnx)
+        # logger.info(f"{site}: {plugin_status}")
+        
+    plugin_status = blogs.activate_plugin('a:1:{i:0;s:33:"classic-editor/classic-editor.php";}', '/', 1,cnx)
+    print(plugin_status)
+    #what are we activating based on? blog_id and then getting serialised plugin string? or just getting the serialised plugin string initially
+
     cnx.close()
 
 if __name__ == "__main__":
@@ -32,6 +45,7 @@ if __name__ == "__main__":
                 password = cfg["password"])
     
     log_file = cfg['log_file']
+
     logger = logging.getLogger(__name__)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh = logging.FileHandler(log_file, mode='w')
--- wordpress.py
+++ wordpress.py
@@ -2,6 +2,8 @@ from typing import List
 import base64
 from colorama import Fore, Back
 import subprocess
+from phpserialize import *
+import json
 
 class wp:
     def __init__(self, url: str = "https://localhost", username: str = "", password: str = "") -> None:
@@ -22,12 +24,59 @@ class wp:
         self.token = base64.b64encode(credentials.encode()).decode('utf-8')
         
 
-    def activate_plugin(self, plugin_slug, site) -> str:
-        p = subprocess.run(f"wp plugin activate {plugin_slug} --path=/var/www/html --url=https://blogs-dev.butler.edu{site}", shell=True, capture_output=True)
-        # print(p.stdout)
-        print(f"{Fore.GREEN}{plugin_slug} was activated on {site}")
-        return p.stdout
+    def activate_plugin(self, plugin, site, site_id,cnx) -> str:
+        plugin_slug = wp.get_plugin_slug(self,plugin,site_id,cnx)
+        if plugin_slug != "":
+            p = subprocess.run(f"wp plugin activate {plugin_slug} --path=/var/www/html --url=https://blogs-dev.butler.edu{site}", shell=True, capture_output=True)
+            # print(p.stdout)
+            print(f"{Fore.GREEN}{plugin_slug} was activated on {site}")
+            return p.stdout
     
+
+    def deactivate_plugin(self, plugin_slug, site) -> str:
+        p = subprocess.run(f"wp plugin deactivate {plugin_slug} --path=/var/www/html --url=https://blogs-dev.butler.edu{site}", shell=True, capture_output=True)
+        # print(p.stdout)
+        print(f"{Fore.GREEN}{plugin_slug} was deactivated on {site}")
+        return p.stdout
+
+
+    def get_plugin_slug(self, plugin, site_id:int, mysql) -> str:
+        cursor = mysql.cursor()
+        
+        query = ('select option_value from wp_%s_options where option_name = "active_plugins"')
+        cursor.execute(query, (site_id,))
+
+        results = cursor.fetchall()
+        site_plugins = []
+
+        for r in results: #creates a list of the site's activated plugins 
+            data = r[0]
+            plugin_dict = loads(data.encode())
+            for p in plugin_dict.keys():
+                plugin_name = plugin_dict[p].decode() #'classic-editor/classic-editor.php'
+                slug = plugin_name.split("/")[0]
+                site_plugins.append(slug)
+        # print(list(plugin_dict.values()))
+
+        data = plugin
+        pl = loads(data.encode())
+        # IF-ELSE: OPPOSITE FOR DEACTIVATE
+        if pl[0] not in list(plugin_dict.values()): #checks if suggested plugin is already activated
+            slug = pl[0].decode()
+            plugin_slug = slug.split("/")[0]
+
+            cursor.close()
+
+            return plugin_slug
+        else:
+            slug = pl[0].decode()
+            plugin_slug = slug.split("/")[0]
+            print(f"{Fore.GREEN}{plugin_slug} was already active{Fore.RESET}")
+
+            cursor.close()
+
+            return ""
+
 
     def get_user_blogs(self, user_blogs, mysql) -> None: 
         """Gets all the blog ids and blog paths.
